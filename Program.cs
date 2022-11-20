429ff8cff73a134d1a6f2bcc77e23f2369f15e37
Даниил Полянский <dm.tsiganov@gmail.com>
2022-11-20 22:04:59 +0700

    2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int a, b, c;
+Console.WriteLine("Введите первое число");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");  
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b>max) max=b;
+if (c>max) max=c;
+Console.Write("max=");
+Console.WriteLine(max);
