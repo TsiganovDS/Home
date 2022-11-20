c3828e27bd7e4d09cebe0876ed2282bd71098284
Даниил Полянский <dm.tsiganov@gmail.com>
2022-11-20 22:08:58 +0700

    1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int a = new int();
+int b = new int();
+Console.WriteLine("Введите первое число");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+b = Convert.ToInt32(Console.ReadLine());
+    bool Chto0 = a > b;
+    bool Chto1 = a < b;
+        if (Chto0)
+    {
+     Console.Write("Max=");
+     Console.WriteLine(a);
+    }
+        
+    else if (Chto1)
+    {
+     Console.Write("Max=");
+     Console.WriteLine(b);
+    }                
